03034d02659c755b67ed7b2f5bc1c181f534078f
nstoyanov613 <nstoyanov5397@stu.d214.org>
2025-05-15 12:36:30 -0500

    Update MyApp.swift
    Added a splash screen.

--- MyApp.swift
+++ MyApp.swift
@@ -2,9 +2,32 @@ import SwiftUI
 
 @main
 struct MyApp: App {
+    
+    @State private var showApp = false
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ZStack {
+                if showApp {
+                    ContentView()
+                        .transition(.opacity)
+                } else {
+                    Image(systemName: "leaf.fill")
+                        .foregroundStyle(.green)
+                        .font(.largeTitle)
+                        .transition(.opacity)
+                        .symbolEffect(.bounce, options: .speed(0.5), isActive: true)
+                }
+            }
+            .animation(.easeInOut(duration: 0.5), value: showApp)
+            .onAppear() {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                    withAnimation {
+                        showApp = true
+                    }
+                }
+            }
+            
         }
     }
 }
